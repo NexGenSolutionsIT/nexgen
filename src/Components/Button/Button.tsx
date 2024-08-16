5c24dd0750857400b89a8b9bc71e80facb3bab83
Maduxt <maria.eduarda2812@hotmail.com>
2024-08-16 18:06:45 -0300

    feat button

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import style from "./button.module.scss";
+interface ButtonProps {
+  label: string;
+  onClick?: () => void;
+  disabled?: boolean;
+  type?: "button" | "submit";
+}
 
-function Button() {
-  return <div>Button</div>;
+function Button({ label, onClick, disabled, type }: ButtonProps) {
+  return (
+    <div>
+      <button
+        className={style.button}
+        onClick={onClick}
+        disabled={disabled}
+        type={type}
+      >
+        {label}
+      </button>
+    </div>
+  );
 }
 
 export default Button;
