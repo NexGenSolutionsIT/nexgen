e472b702e16eda1517bff303759e4bb607360155
Pixel Chef <carlos.hcal@gmail.com>
2024-08-19 17:15:02 -0300

    feat link component

--- src/Components/Links/Links.tsx
+++ src/Components/Links/Links.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import dark from "./styles/dark.module.scss";
+import light from "./styles/light.module.scss";
+import Link from "next/link";
 
-function Links() {
-  return <div>Links</div>;
+interface LinksProps {
+  text: string;
+  variant?: "dark" | "light";
+  href: string;
+}
+
+function Links({ variant = "light", text, href }: LinksProps) {
+  const getStyles = () => {
+    switch (variant) {
+      case "dark":
+        return dark;
+      default:
+        return light;
+    }
+  };
+
+  const variantStyles = getStyles();
+
+  return (
+    <div>
+      <Link href={href} className={variantStyles.link}>
+        {text}
+      </Link>
+    </div>
+  );
 }
 
 export default Links;
