3089946256127bfa3dc21fe25da80757ef69f5f7
Maduxt <maria.eduarda2812@hotmail.com>
2024-08-19 16:38:46 -0300

    fix button

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,17 +1,35 @@
 import React from "react";
-import style from "./button.module.scss";
+import normal from "./styles/normal.module.scss";
+import reverse from "./styles/reverse.module.scss";
 interface ButtonProps {
   label: string;
   onClick?: () => void;
   disabled?: boolean;
   type?: "button" | "submit";
+  variant?: "reverse" | "normal";
 }
 
-function Button({ label, onClick, disabled, type }: ButtonProps) {
+function Button({
+  label,
+  onClick,
+  disabled,
+  type,
+  variant = "normal",
+}: ButtonProps) {
+  const getStyles = () => {
+    switch (variant) {
+      case "reverse":
+        return reverse;
+      default:
+        return normal;
+    }
+  };
+  const variantStyles = getStyles();
+
   return (
     <div>
       <button
-        className={style.button}
+        className={variantStyles.button}
         onClick={onClick}
         disabled={disabled}
         type={type}
