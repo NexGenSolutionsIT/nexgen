86a36e6aab9b89ffaa69483403bad24fcdafb9fb
Maduxt <maria.eduarda2812@hotmail.com>
2024-08-19 19:43:46 -0300

    feat toggle theme

---
+++ src/Components/ToggleTheme/ToggleTheme.tsx
@@ -0,0 +1,40 @@
+"use client";
+import React from "react";
+import { useTheme } from "../../Context/ThemeContext";
+import styles from "./toggleTheme.module.scss";
+import {
+  IoSunnyOutline,
+  IoSunny,
+  IoMoon,
+  IoMoonOutline,
+} from "react-icons/io5";
+
+function ThemeToggleButton() {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <button onClick={toggleTheme} className={styles.button}>
+      {theme === "light" ? (
+        <span>
+          <span className={`${styles.iconNormal} ${styles.lightIcon}`}>
+            <IoMoonOutline />
+          </span>
+          <span className={`${styles.iconHover} ${styles.lightIconHover}`}>
+            <IoMoon />
+          </span>
+        </span>
+      ) : (
+        <span>
+          <span className={`${styles.iconNormal} ${styles.darkIcon}`}>
+            <IoSunnyOutline />
+          </span>
+          <span className={`${styles.iconHover} ${styles.darkIconHover}`}>
+            <IoSunny />
+          </span>
+        </span>
+      )}
+    </button>
+  );
+}
+
+export default ThemeToggleButton;
