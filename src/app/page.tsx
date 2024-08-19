0c53d97228ff56518666bc0806221ec1afe7d904
Maduxt <maria.eduarda2812@hotmail.com>
2024-08-19 19:43:23 -0300

    fix page app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import "@/styles/global.scss";
 import "@/styles/reset.scss";
 import Home from "./Home/page";
+import { ThemeProvider } from "@/Context/ThemeContext";
 
 export default function App() {
   return (
     <>
-      <Home />
+      <ThemeProvider>
+        <Home />
+      </ThemeProvider>
     </>
   );
 }
