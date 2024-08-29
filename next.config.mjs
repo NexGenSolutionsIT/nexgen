93bc59cec9571b5c8aff78684139433a8fadb5a7
Pixel Chef <carlos.hcal@gmail.com>
2024-08-29 18:54:56 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  pageExtensions: ["page.tsx", "tsx", "js", "jsx"],
+  reactStrictMode: true,
+};
 
 export default nextConfig;
