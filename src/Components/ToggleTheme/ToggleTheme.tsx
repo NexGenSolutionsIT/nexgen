2f8c4802abe6dc0fa9b5985881b3f3da7641c183
Pixel Chef <carlos.hcal@gmail.com>
2024-08-29 18:56:13 -0300

    Update ToggleTheme.tsx

--- src/Components/ToggleTheme/ToggleTheme.tsx
+++ src/Components/ToggleTheme/ToggleTheme.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { useTheme } from "../../Context/ThemeContext";
+import { useTheme } from "../../context/ThemeContext";
 import styles from "./toggleTheme.module.scss";
 import {
   IoSunnyOutline,
