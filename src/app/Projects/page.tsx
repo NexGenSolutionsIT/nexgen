16afc7f508391806117724475ea11ba435ef18fb
Pixel Chef <carlos.hcal@gmail.com>
2024-08-29 18:55:49 -0300

    Update page.tsx

--- src/app/Projects/page.tsx
+++ src/app/Projects/page.tsx
@@ -1,19 +1,31 @@
 "use client";
 import React from "react";
-import { useTheme } from "@/Context/ThemeContext";
+import { useTheme } from "@/context/ThemeContext";
 import dark from "./styles/dark.module.scss";
 import light from "./styles/light.module.scss";
 import Card from "./Card/Card";
-import youtube from "../../../public/images/logos_youtube.svg";
-import neeforex from "../../../public/images/neeforex.png";
+import Link from "next/link";
+import projects from "../../../projects.json";
 import neeforexLight from "../../../public/images/neeforex-light.png";
-import nexgen from "../../../public/images/nexgen.svg";
-import nexgenLight from "../../../public/images/nexgen-light.svg";
+import neeforexDark from "../../../public/images/neeforex.png";
 import cryptoWalletLight from "../../../public/images/crypto-wallet-light.png";
-import cryptoWallet from "../../../public/images/crypto-wallet.png";
+import cryptoWalletDark from "../../../public/images/crypto-wallet.png";
+import nexgenLight from "../../../public/images/nexgen-light.svg";
+import nexgenDark from "../../../public/images/nexgen.svg";
+import { StaticImageData } from "next/image";
+
+const images: Record<ImageKey, StaticImageData> = {
+  neeforexLight,
+  neeforexDark,
+  cryptoWalletLight,
+  cryptoWalletDark,
+  nexgenLight,
+  nexgenDark,
+};
 
 function Projects() {
   const { theme } = useTheme();
+
   const getStyles = () => {
     switch (theme) {
       case "light":
@@ -22,81 +34,42 @@ function Projects() {
         return dark;
     }
   };
-  const variantStyles = getStyles();
 
-  const cardData = [
-    {
-      project: "Nee Forex",
-      href: "https://neeforex.vercel.app/",
-      image: theme === "light" ? neeforexLight : neeforex,
-      alt: "Nee forex",
-      link: "neeforex.com",
-    },
-    {
-      project: "Crypto Wallet",
-      href: "https://wallet-crypto-tau.vercel.app/",
-      image: theme === "light" ? cryptoWalletLight : cryptoWallet,
-      alt: "Wallet Crypto",
-      link: "cryptoWallet.com",
-    },
-    {
-      project: "YouTube",
-      href: "https://www.youtube.com/",
-      image: youtube,
-      alt: "youtube",
-      link: "youtube.com",
-    },
-    {
-      project: "Nexgen IT Solutions",
-      href: "https://nexgen-rho.vercel.app/",
-      image: theme === "light" ? nexgenLight : nexgen,
-      alt: "Nexgen IT Solutions",
-      link: "nexgenitsolutions.com",
-    },
-    {
-      project: "YouTube",
-      href: "https://www.youtube.com/",
-      image: youtube,
-      alt: "youtube",
-      link: "youtube.com",
-    },
-    {
-      project: "YouTube",
-      href: "https://www.youtube.com/",
-      image: youtube,
-      alt: "youtube",
-      link: "youtube.com",
-    },
-    {
-      project: "YouTube",
-      href: "https://www.youtube.com/",
-      image: youtube,
-      alt: "youtube",
-      link: "youtube.com",
-    },
-  ];
+  const variantStyles = getStyles();
 
   return (
     <div className={variantStyles.container}>
       <div className={variantStyles.title_subtitle}>
         <p className={variantStyles.title}>
-          / <span className={variantStyles.title_span}>Projects</span>.
+          / <span className={variantStyles.title_span}>Projetos</span>.
         </p>
         <p className={variantStyles.subtitle}>
           Nossos projetos com maior destaque.
         </p>
 
         <div className={variantStyles.cards}>
-          {cardData.map((card, index) => (
-            <Card
-              alt={card.alt}
-              href={card.href}
-              image={card.image}
-              link={card.link}
-              project={card.project}
-              key={index}
-            />
-          ))}
+          {projects.map((card, index) => {
+            const imageKey =
+              theme === "light"
+                ? (card.imageLight as ImageKey)
+                : (card.imageDark as ImageKey);
+
+            return (
+              <Link
+                href={`/projects/project/${card.id}`}
+                key={index}
+                className={variantStyles.link}
+              >
+                <Card
+                  alt={card.alt}
+                  href={card.href}
+                  image={images[imageKey]}
+                  link={card.link}
+                  project={card.project}
+                />
+              </Link>
+            );
+          })}
         </div>
       </div>
     </div>
