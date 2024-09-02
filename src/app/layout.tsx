218e4c63d6cca723cbf73d702876d1dc056e3f6a
Pixel Chef <carlos.hcal@gmail.com>
2024-09-02 18:04:02 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from "@/tempComponents/Footer/Footer";
-import Header from "@/tempComponents/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
 import { ThemeProvider } from "@/Context/ThemeContext";
 import type { Metadata } from "next";
 import "@/styles/global.scss";
