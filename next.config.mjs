3f9a579052b1371090c8c1a2457531f13c9758aa
Pixel Chef <carlos.hcal@gmail.com>
2024-09-02 17:42:31 -0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,37 @@
 const nextConfig = {
   pageExtensions: ["page.tsx", "tsx", "js", "jsx"],
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.cache = {
+        type: "filesystem",
+        allowCollectingMemory: true,
+        buildDependencies: {
+          config: [__filename],
+        },
+      };
+    }
+
+    // Desativar o cache para loaders problemáticos
+    config.module.rules.forEach((rule) => {
+      if (Array.isArray(rule.use)) {
+        rule.use.forEach((use) => {
+          if (
+            typeof use === "object" &&
+            use.loader &&
+            use.loader.includes("sass-loader")
+          ) {
+            use.options = {
+              ...use.options,
+              cache: false,
+            };
+          }
+        });
+      }
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
