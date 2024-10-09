8f8b1682e19f949cafd6a1ab37a40eef9da6cd2b
Pixel Chef <carlos.hcal@gmail.com>
2024-10-09 00:54:29 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Hero from "./Hero/Hero";
-import SectionOne from "./SectionOne/SectionOne";
+
 import Contact from "@/components/Contact/Contact";
 import styles from "./styles.module.scss";
-import SectionTwo from "./SectionTwo/SectionTwo";
-import SectionTree from "./SectionTree/SectionTree";
+import Hero from "./Hero/asdasdasd";
+import SectionOne from "./SectionOne/asdasdasdasd";
+import SectionTwo from "./SectionTwo/asdasdasdasd";
+import SectionTree from "./SectionTree/asdasdasdasd";
 
 function Home() {
   return (
