5224588658e8b0c2bf6969521213971250c64308
Pixel Chef <carlos.hcal@gmail.com>
2024-10-08 23:25:45 -0300

    Update page.tsx

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import Hero from "./Hero/Hero";
 import SectionOne from "./SectionOne/SectionOne";
 import Contact from "@/components/Contact/Contact";
-import styles from "./styles.module.scss";
+// import styles from "./styles.module.scss";
 
 function Home() {
   return (
     <div>
       <Hero />
       <SectionOne />
-      <div className={styles.container}>
+      {/* <div className={styles.container}>
         <Contact />
-      </div>
+      </div> */}
     </div>
   );
 }
